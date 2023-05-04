efd33b62c32c41999498c3e9641ab8a6a3186dee
Sergio1269529 <1269529@alu.murciaeduca.es>
2023-05-04 16:04:17 +0200

    Inicio Proyecto

---
+++ src/ejercicioper1/EJERCICIOPER1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicioper1;
+
+/**
+ *
+ * @author Sergio
+ */
+public class EJERCICIOPER1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
